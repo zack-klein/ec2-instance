4873620fd85ee9ac0e5d466945b0b4e8e103badf
zack-klein <klein.zachary.j@gmail.com>
2020-10-02 17:21:48 -0400

    feat: Add security group to module outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "autoscaling_group" {
   value       = aws_autoscaling_group.instance
   description = "Auto scaling group for the instance."
 }
+
+output "security_group" {
+  value = aws_security_group.security_group
+  description = "Auto scaling group's security group."
+}
